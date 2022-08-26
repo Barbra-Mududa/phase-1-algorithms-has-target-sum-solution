b543852972b2111de8b1fd58da494c3a965c2f33
Barbra Mududa <barbra.mududa@student.moringaschool.com>
2022-08-27 01:14:30 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,15 +1,27 @@
 function hasTargetSum(array, target) {
   // Write your algorithm here
+ for(let i = 0; i < array.length; i++){
+  const complement = target - array[i];
+  for(let j=i+1;j< array.length; j++ ){
+    if(array[j] === complement)
+    return true;
+  }
+ }
+ return false
 }
 
 /* 
   Write the Big O time complexity of your function here
 */
-
+// 0(n)
 /* 
   Add your pseudocode here
 */
-
+// iterate over array
+// identify number that adds to our target
+// iterate over remaining array
+// if any number gives desired XPathResult, reurn true
+// if there is no desired value at the end of the array return false
 /*
   Add written explanation of your solution here
 */
